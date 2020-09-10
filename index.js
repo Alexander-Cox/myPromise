7f9b8cfbc8dfcc0d2c1233c04edda07b26740ac9
Alexander Cox <alexander.b.cox@outlook.com>
2020-09-10 17:44:29 +0100

    is this progress... no idea

--- index.js
+++ index.js
@@ -1,85 +1,71 @@
+const myPromise = require('./my-promise');
+
 //temp rough
 function resolve(value) {
-  this.status = "fulfilled";
+  this.status = 'fulfilled';
   this.value = value;
 }
 
 function reject(value) {
-  this.status = "rejected";
+  this.status = 'rejected';
   this.value = value;
 }
 
-const defaultProm = {
-  name: "default",
-  status: "pending",
-  value: undefined,
-  resolve: resolve,
-  init: function () {
-    setTimeout(() => {
-      this.resolve("orange");
-    }, 0);
-    return this;
-  },
-  then: then,
-};
-
-const promOne = {
-  name: 1,
-  status: "pending",
-  value: undefined,
-  resolve: resolve,
-  init: function () {
-    setTimeout(() => {
-      this.resolve("banana");
-    }, 0);
-  },
-  then: then,
-};
-
-const promTwo = {
-  name: 2,
-  status: "pending",
-  value: undefined,
-  resolve: resolve,
-  init: function () {
-    setTimeout(() => {
-      this.resolve("apple");
-    }, 0);
-    return this;
-  },
-  then: then,
-};
-
-function then(cb) {
-  // console.log(".........", this.name, "calling then.......\n");
-  const defaultPromise = defaultProm.init();
-  //if previous promise
-  if (this.name === "default") {
-    console.log("we need to wait");
-    setImmediate(() => {
-      this.then(cb);
-    });
-  }
-  //stop this from firing before previous promise (orange is bad, we want apple)
-  //find a way to pass the result from previous onFulfilment promise
-  if (this.status === "pending") {
-    setImmediate(() => {
-      this.then(cb);
-    });
-  } else {
-    console.log(this.name, "then value", this.value);
-  }
-  return defaultPromise;
+function createPromise(name, output) {
+  const prom = {
+    resolve,
+    reject,
+    name,
+    then,
+    status: 'pending',
+    value: undefined,
+    init: function () {
+      setTimeout(() => {
+        this.resolve(output);
+      }, 0);
+      return this;
+    },
+  };
+  return prom.init();
 }
 
-promOne.init();
+const defaultProm = createPromise('default', 'orange');
+const promOne = createPromise('ONE', 'banana');
+const promTwo = createPromise('TWO', 'apple');
+
+function then(cb) {
+  //pass a new promise to allow further chaining
+  //invoke cb with p.value if p.status is fulfilled
+  //use cb return value to decide value of then then of already returned promise
+
+  //stop this from firing before previous promise (orange is bad, we want apple)
+  //find a way to pass the result from previous onFulfilment promise
+  if (this.status === 'fulfilled') {
+    if (this.name === 'default') {
+      return;
+    } else {
+      const result = cb(this.value);
+      if (result instanceof myPromise) return myPromise;
+    }
+    console.log(this.name, 'then finished value:', this.value);
+  }
+  if (
+    (this.status === 'pending' && this.name === 'default') ||
+    this.status === 'pending'
+  ) {
+    setImmediate(() => {
+      this.then(cb);
+    });
+  }
+  return defaultProm;
+}
 
 const applePromise = promOne.then((result) => {
-  console.log("is this banana: ", result);
+  console.log('is this banana: ', result);
   return promTwo.init();
 });
 
 console.log(applePromise);
 applePromise.then((result) => {
-  console.log("is this apple: ", result);
+  console.log('is this apple: ', result);
 });
--- my-promise.js
+++ my-promise.js
@@ -1,63 +1,63 @@
 class myPromise {
   constructor(executor) {
-    this.__status = "pending";
+    this.__status = 'pending';
     this.__value = undefined;
-    executor(
-      (fulfillmentValue) => {
-        this.__status = "fulfilled";
-        this.__value = fulfillmentValue;
-      },
-      (rejectedValue) => {
-        this.__status = "rejected";
-        this.__value = rejectedValue;
-      }
-    );
+    this.__thenCallbacks = [];
+    this.__propogatedResolve = undefined;
+    this.__propogatedReject = undefined;
+    executor(this.__resolve.bind(this), this.__reject.bind(this));
+  }
+
+  __resolve(fulfillmentValue) {
+    this.__status = 'fulfilled';
+    this.__value = fulfillmentValue;
+  }
+
+  __reject(rejectedValue) {
+    this.__status = 'rejected';
+    this.__value = rejectedValue;
   }
 
   then(onFulfillment) {
-    if (this.__status === "pending") {
-      setImmediate(() => {
-        this.then(onFulfillment);
-      });
-    }
-    // else if (this.__status === "fulfilled") {
-    //   const onFulfillmentResult = onFulfillment(this.__value);
-    //   return onFulfillmentResult;
-    // }
-    return new myPromise((resolve, reject) => {
-      setImmediate(() => {
-        const onFulfillmentResult = onFulfillment(this.__value);
-        if (onFulfillmentResult instanceof myPromise) {
-          // this = onFulfillmentResult;
+    setImmediate(() => {
+      //if promise is pending...
+      //exit and put a new then on the immediate queue
+      if (this.__status === 'pending') {
+        return setImmediate(() => {
+          this.then(onFulfillment);
+        });
+      } else {
+        //otherwise we must be resolved/rejected
+        //get result back from then callback
+        const result = onFulfillment(this.__value);
+        if (result instanceof myPromise) {
+          return this.__propogatedResolve(result.__value);
         } else {
-          resolve(onFulfillmentResult);
+          return this.__propogatedResolve(result);
         }
-      });
+      }
     });
-    // if (this.__status === "fulfilled") {
-    //   const onFulfillmentResult = process.nextTick(() =>
-    //     onFulfillment(this.__value)
-    //   );
-    //   if (onFulfillmentResult instanceof myPromise) return onFulfillmentResult;
-    //   else
-    //     return new myPromise((resolve) => {
-    //       resolve(onFulfillmentResult);
-    //     });
-    // } else if (this.__status === "rejected") return this;
-    // return new myPromise(() => {});
+    //if we have no new promise resolve function
+    //then we havent creates a promise for the sync code
+    if (!this.__propogatedResolve) {
+      //create a promise for the purpose of chaining future
+      const newProm = new myPromise(() => {});
+
+      this.__propogatedResolve = this.__resolve.bind(newProm);
+      //return promise for chaining next then on
+      return newProm;
+    }
+    //this return only matters in the first run since other calls with by async
   }
 
   catch(onRejection) {
-    if (this.__status === "pending") {
+    if (this.__status === 'pending') {
       setImmediate(() => {
         this.catch(onRejection);
       });
-    } else if (this.__status === "rejected") {
+    } else if (this.__status === 'rejected') {
       onRejection(this.__value);
     }
-    // if (this.__status === "rejected") onRejection(this.__value);
-    // if (this.__status === "fulfilled") return this;
-    // return new myPromise(() => {});
   }
 
   static all(promises) {
